1cfac204fc10a64e9285f813f21cf658ad7fb3ee
medmouheb <mohamedmouheb@gmail.com>
2025-03-24 01:22:16 +0100

    first commit

---
+++ src/main/java/com/example/KDRIVE/KdriveApplication.java
@@ -0,0 +1,13 @@
+package com.example.KDRIVE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KDRIVE/KdriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KDRIVE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
